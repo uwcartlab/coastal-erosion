4a63bfdb3d57c8e9a53fd112c7c186f44a3f36b7
Brady Sheridan <brady.sherid@gmail.com>
2018-10-22 20:55:24 -0500

    update header/footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,7 @@ import ReactMarkdown from 'react-markdown'
 export default () => (
   <SiteData
     render={({footerContent}) => (
-      <div
-        className="footer-wrap"
-        style={{display: 'flex', alignItems: 'flex-start', flexWrap: 'wrap', width: '100%', padding: '30px 0px 50px 0px', backgroundColor: '#232323'}}
-      >
+      <div id="footer-wrap">
         {
           Object.keys(footerContent).map((k) => (
             <div>
