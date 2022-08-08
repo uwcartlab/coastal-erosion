17faadd31104501b7bced19045bcf3ac53f15565
cartobaldrica <baldrica18@gmail.com>
2022-08-08 15:36:14 -0500

    create working version of buffer calculation
    -created clone function to make a deep copy of the data for buffering
    -created function to convert feet to degrees longitude, which is then used to create the buffer line

--- js/calculator.js
+++ js/calculator.js
@@ -4,6 +4,8 @@
 
 (function(){
     
+    var map, data, buffer;
+
     function setListeners(){
         document.getElementById("calcSetback").addEventListener("click", function(){
             calcSetback();
@@ -12,36 +14,33 @@
     
     function createMap(){
         //map variable
-        let map = L.map('map').setView([43.3202, -87.9256], 13);
+        map = L.map('map').setView([43.3202, -87.9256], 13);
         //openstreetmap basemap
         var basemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
             attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
         }).addTo(map);
         //add bluff crest data
-        addData(map);
+        addData();
     }
 
-    function addData(map){
-        let crest, bufferData, buffer;
+    function addData(){
+        let crest;
         
         fetch("data/Grafton_BluffCrest.geojson")
             .then(res => res.json())
-            .then(function(data){
-                crest = L.geoJSON(data).addTo(map);
+            .then(function(res){
+                crest = L.geoJSON(res).addTo(map);
 
-                bufferData = data;
-                bufferData.features[0].geometry.coordinates.forEach(function(coord){
-                    coord.forEach(function(latlng){
-                        latlng[0] = latlng[0] - 0.02
-                    })
-                })
-
-                buffer = L.geoJSON(bufferData).addTo(map);
+                data = res;
             })
     }
 
     // This function calculates the setback based on user input
     function calcSetback(){
+        //remove buffer layer if already enabled
+        if (buffer){
+            map.removeLayer(buffer);
+        }
         // read in input and format correctly
         let localReg = parseInt(document.getElementById('local_reg').value),
             height = parseInt(document.getElementById('bluff_height').value),
@@ -61,6 +60,52 @@
         //calculate the total setback in ft(example should be 199 [25 + 74 + 100])
         let setback = parseInt(localReg + sas + rec_set);
         document.getElementById('setback_ft').value = setback;
+
+        //create temp variable to store buffer data
+        let bufferData = clone(data);
+
+        //create buffer line
+        bufferData.features[0].geometry.coordinates.forEach(function(coord){
+            //convert setback distance into degrees 
+            coord.forEach(function(latlng){
+                //conversion factor for 1 mile, 69.172 = length of a degree of longitude at equator
+                let cf = (Math.cos(latlng[1]) * 69.172) * 5280;                  
+                //displace line based on calculaed conversion factor
+                latlng[0] = latlng[0] - (setback/cf);
+            })
+        })
+        //add buffer line to map
+        buffer = L.geoJSON(bufferData).addTo(map);
+    }
+    //function clone data variable to store in new layer for buffer calculation
+    function clone(Obj){
+        //cloned object
+        let buf; 
+        //if variable is an array
+        if (Obj instanceof Array) {
+            //create an empty array
+            buf = []; 
+            var i = Obj.length;
+            while (i--) {
+                //recursively clone the elements
+                buf[i] = clone(Obj[i]); 
+            }
+            return buf;
+        //if variable is an object
+        } else if (Obj instanceof Object) {
+            //create an empty object
+            buf = {}; 
+            for (const k in Obj) {
+                //filter out another array's index
+                if (Obj.hasOwnProperty(k)) { 
+                     //recursively clone the value
+                    buf[k] = clone(Obj[k]);
+                }
+            }
+            return buf;
+        } else {
+            return Obj;
+        }
     }
 
     document.addEventListener('DOMContentLoaded', function(){
